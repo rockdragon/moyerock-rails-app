bbceb1030445a6a3db4358496ee12aeb65290d2c
Locke <moyerock@gmail.com>
2016-10-20 00:03:26 +0800

    chore: add about & remove home

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
