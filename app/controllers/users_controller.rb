31f7afd95e282c2a66c86c111b3960f81962fc17
Locke <moyerock@gmail.com>
2016-10-25 15:42:18 +0800

    feat: user login logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash.now[:success] = 'user was created successfully.'
       redirect_to @user
     else
