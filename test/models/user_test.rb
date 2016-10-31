61a1a95518e7e63ba4267025dc818bd5e81a411b
Locke <moyerock@gmail.com>
2016-10-31 15:17:03 +0800

    chore: user destroy DONE

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,61 +3,61 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com',
-                     password: 'foobar', password_confirmation: 'foobar')
+    @admin = User.new(name: 'Example User', email: 'user@example.com',
+                      password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
-    assert @user.valid?
+    assert @admin.valid?
   end
 
   test 'name should be present' do
-    @user.name = ''
-    assert_not @user.valid?
+    @admin.name = ''
+    assert_not @admin.valid?
   end
 
   test 'email should be present' do
-    @user.email = '     '
-    assert_not @user.valid?
+    @admin.email = '     '
+    assert_not @admin.valid?
   end
 
   test 'name should not be too long' do
-    @user.name = 'a' * 51
-    assert_not @user.valid?
+    @admin.name = 'a' * 51
+    assert_not @admin.valid?
   end
 
   test 'email should not be too long' do
-    @user.email = 'a' * 244 + '@example.com'
-    assert_not @user.valid?
+    @admin.email = 'a' * 244 + '@example.com'
+    assert_not @admin.valid?
   end
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_email|
-      @user.email = invalid_email
-      assert_not @user.valid?, "#{invalid_email} should be invalid."
+      @admin.email = invalid_email
+      assert_not @admin.valid?, "#{invalid_email} should be invalid."
     end
   end
 
   test 'email addresses should be unique' do
-    duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    @user.save
+    duplicate_user = @admin.dup
+    duplicate_user.email = @admin.email.upcase
+    @admin.save
     assert_not duplicate_user.valid?
   end
 
   test 'password should be present (nonblank)' do
-    @user.password = @user.password_confirmation = ' ' * 6
-    assert_not @user.valid?
+    @admin.password = @admin.password_confirmation = ' ' * 6
+    assert_not @admin.valid?
   end
 
   test 'password should be have a minimum length' do
-    @user.password = @user.password_confirmation = 'a' * 5
-    assert_not @user.valid?
+    @admin.password = @admin.password_confirmation = 'a' * 5
+    assert_not @admin.valid?
   end
 
   test 'authenticated? should return false for a user with nil digest' do
-    assert_not @user.authenticated?('')
+    assert_not @admin.authenticated?('')
   end
 end
